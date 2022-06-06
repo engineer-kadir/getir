d40df66d3ef788d322aa5b95daa768c3e704ac1a
engineer-kadir <kadiryildizmeb@gmail.com>
2022-06-06 14:34:06 +0300

    Work on Hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,17 @@ module.exports = {
   theme: {
     extend: {
       colors:{
-        'brand-color':'#5d3ebc'
+        'brand-color':'#4c3398',
+        'brand-yellow':'#ffd300',
+        'primary-brand-color':'#5d3ebc',
+        'secondary-brand-color':'#7849f7',
+        'facebook-color':'#1877f2',
+      },
+
+      gradientColorStops:{
+        'brand-color':'#4c3398',
+        'primary-brand-color':'#5d3ebc',
+        'secondary-brand-color':'#7849f7'
       },
     },
   },
